c5201c586860ae59772a18cff349ae2e349a7bb8
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-10-02 10:51:36 -0300

    adicão do modal, e dos estilo do modal.

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,15 @@
+// Classe para representar um Pokémon
+class Pokemon {
+    constructor() {
+        this.number = '';
+        this.name = '';
+        this.type = '';
+        this.types = [];
+        this.photo = '';
+        this.stats = [];
+        this.abilities = [];
+        this.species = '';
+        this.height = '';
+        this.weight = '';
+    }
+}
